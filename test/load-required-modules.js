4e720264c4b2f46c31ac0705dfe63db7f36e6d96
Mohamed Elsayed <me@mohamedelsayed.net>
2022-08-26 08:19:26 +0200

    make validation using joi

--- test/load-required-modules.js
+++ test/load-required-modules.js
@@ -1,10 +1,15 @@
 'use strict';
 
+require('dotenv').config();
 require('ts-node').register({
 	project: 'tsconfig.json',
 	files: true
 });
 const chai = require('chai');
+// const sinonChai = require('sinon-chai');
 const chaiAsPromised = require('chai-as-promised');
+// const chaiDateTime = require('chai-datetime');
 
+// chai.use(sinonChai);
 chai.use(chaiAsPromised);
+// chai.use(chaiDateTime);
