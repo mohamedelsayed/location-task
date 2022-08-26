2705e4eff934ab2552c77ef3de8f2eb689d47597
Mohamed Elsayed <me@mohamedelsayed.net>
2022-08-26 08:19:10 +0200

    make validation using joi

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ import {MqttConnector} from './connectors/MqttConnector';
 import {createDBConnection} from './helpers/db';
 import {logger} from './helpers/logger';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const dbConnection = createDBConnection(logger);
 
 const mqttConnector = new MqttConnector(logger);
